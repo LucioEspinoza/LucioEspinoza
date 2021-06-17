38816b0a0d3c3979e4d1714adb3a4d8c8f5d940b
Lucio Espinoza <lufespin@ucsc.edu>
2021-06-17 08:54:21 -0700

    Updated page files

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const AboutPage = () =>{
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <p>Shababy Shabooby</p>
+        </main>
+    )
+}
+
+export default AboutPage;
